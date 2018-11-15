8417fee6a6cdd665b46266bc4a74b9073685e385
Frestar Haruo Suzuki <suzuki@frestar.co.jp>
2018-11-15 12:02:45 +0900

    速度の記述変更と、コメントの追加

--- Assets/Script/BackgroundController.cs
+++ Assets/Script/BackgroundController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class BackgroundController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(0, -0.03f*60*Time.deltaTime, 0);
-        if (transform.position.y < -9.8f) {
-            float y = transform.position.y;
-            transform.position = new Vector3(0, y + 19.6f, 0);
+    // BGの移動速度は一定なので事前に計算しておく。単位はm/s
+    // BGの移動速度＝方向＊スカラ量
+    Vector3 velocity = Vector3.down  * 1.8f;
+
+    float imageHeight = 9.8f; // BGに使われているイメージの縦サイズ
+
+    // Update is called once per frame
+    void Update () {
+
+        // 瞬間的な移動量は時間から算出する。
+        Vector3 deltaVelocity = velocity * Time.deltaTime;
+        transform.Translate(deltaVelocity, Space.World);
+
+        // イメージの巻き戻しチェック。
+        if (transform.position.y < -imageHeight) {
+            // 画面外に出たら巻き戻し。
+            float y = transform.position.y + (imageHeight * 2); // リセット位置の算出。誤差込み。
+            transform.position = new Vector3(0, y, 0);          // リセット位置の設定。
         }
 	}
 }
